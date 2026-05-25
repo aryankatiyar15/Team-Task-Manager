f4f67bfea77e8163dd0146495ca0785cdfe9bda0
Aryan Katiyar <2k22.cse.2213298@gmail.com>
2026-05-25 17:59:26 +0530

    Serve frontend from backend

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,21 @@
+import express from "express";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { env } from "./config/env.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const clientDistPath = path.join(__dirname, "../../client/dist");
+
+app.use(express.static(clientDistPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientDistPath, "index.html"));
+});
 
 async function startServer() {
   try {
